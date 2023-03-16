a68cbdd35f6fba144349deffa40c7e79ee3f59cd
Shubham Jaiswal <shubhujais1995@gmail.com>
2023-03-17 04:26:10 +0530

    remove console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
 
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,10 +13,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    BooksModule,
-    MatButtonToggleModule,
-    BrowserAnimationsModule
+    BooksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
